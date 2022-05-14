55dcd0ddeba1df84de63d989fa2dde7d8eaf9d83
bobby <vilie0215@gmail.com>
2022-05-14 20:05:05 +0300

    somitheng

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void PrintUsageAndExit()
+        {
+            Console.WriteLine("Usage: dotnet run hub-host hub-port owner index");
+            System.Environment.Exit(1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+}
